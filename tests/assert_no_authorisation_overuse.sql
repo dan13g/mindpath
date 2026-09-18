ce8fdd0c0a74fc150aaa3b68752ae0081dfe39da
dan13g <bgheoca@gmail.com>
2026-09-18 11:10:52 +0100

    wip

--- tests/assert_no_authorisation_overuse.sql
+++
@@ -1,3 +0,0 @@
-select *
-from {{ ref('bv_authorisation_utilisation') }}
-where remaining_sessions < 0
--- tests/assert_no_completed_sessions_without_clinician.sql
+++
@@ -1,5 +0,0 @@
--- Returns failing rows. A completed treatment session should normally have a clinician.
-select *
-from {{ ref('fact_session') }}
-where completed_session_count = 1
-  and clinician_key is null
